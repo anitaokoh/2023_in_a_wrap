e85ca77aa1f7919d10bfbc2ebdec9f3d8492908e
Anita Okoh <sleekymeeratura@gmail.com>
2024-12-26 09:31:35 +0100

    fix test bugs

--- src/download_viz.py
+++ src/download_viz.py
@@ -17,5 +17,5 @@ def get_table_download_link(data):
     figure = plotly.io.to_image(data,format="png",  engine="kaleido")
 #     figure =  py.get(data)
     b64 = base64.b64encode(figure).decode()
-    href = f'<a href="data:file/image;base64,{b64}" download="My year in a wrap.png">Download chart</a>'
+    href = f'<a href="data:file/image;base64,{b64}" download="my_year_in_a_wrap.png">Download chart</a>'
     return href
